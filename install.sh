3d395f91b8c19e074ca0167a846f878009c33b00
Chavis Rujichan <chavis.rujichan@gmail.com>
2025-02-22 12:13:37 +0700

    update

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ pacman -Syy
 pacman -S os-prober iwd-dinit resolvconf ntp-dinit linux-firmware linux-headers nano \
     amd-ucode pulseaudio pavucontrol bspwm sxhkd polybar alacritty polkit-gnome \
     rofi picom xorg-server xorg-xinit xorg-xset ttf-jetbrains-mono ttf-jetbrains-mono-nerd \
-    feh pcmanfm
+    feh pcmanfm dunst
 
 # yay -S rofi-greenclip gruvbox-material-gtk-theme-git gruvbox-material-icon-theme-git
