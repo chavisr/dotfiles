2c62055350f22272df61bfb3aaff4712a47e3722
Chavis Rujichan <chavis.rujichan@gmail.com>
2025-07-05 23:16:47 +0700

    update install script

--- .config/install.sh
+++ .config/install.sh
@@ -2,9 +2,19 @@
 
 useradd -m -G wheel -s /bin/bash chavi
 
-pacman -S os-prober iwd-dinit resolvconf ntp-dinit linux-firmware linux-headers nano \
-    amd-ucode pulseaudio pavucontrol bspwm sxhkd polybar alacritty polkit-gnome \
-    rofi picom xorg-server xorg-xinit xorg-xset xorg-xrandr ttf-jetbrains-mono \
-    ttf-jetbrains-mono-nerd feh pcmanfm dunst bash-completion git lxappearance
+# common
+sudo pacman -S os-prober iwd-dinit resolvconf ntp-dinit linux-firmware linux-headers \
+    nano amd-ucode alacritty polkit-gnome ttf-jetbrains-mono ttf-jetbrains-mono-nerd \
+    pcmanfm dunst bash-completion git
 
-yay -S rofi-greenclip gruvbox-material-gtk-theme-git gruvbox-material-icon-theme-git
+yay -S gruvbox-material-gtk-theme-git gruvbox-material-icon-theme-git google-chrome
+
+# bspwm
+sudo pacman -S xorg-server xorg-xinit xorg-xset xorg-xrandr bspwm sxhkd feh polybar \
+    pulseaudio pavucontrol rofi picom lxappearance
+
+yay -S rofi-greenclip
+
+# sway
+sudo pacman -S sway waybar pipewire wireplumber fuzzel xorg-xwayland autotiling \
+    xdg-desktop-portal-wlr pwvucontrol
