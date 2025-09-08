9661374b3453910d4174f09fc9d6bcd26469bda5
Chavis Rujichan <chavis.rujichan@gmail.com>
2025-09-08 17:01:59 +0700

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,19 @@
-vim.opt.termguicolors = true
-vim.cmd("colorscheme gruvbox-material")
-vim.opt.background = "dark"
-vim.opt.number = true
-vim.opt.relativenumber = true
+local vim = vim
+local Plug = vim.fn["plug#"]
 
+vim.call("plug#begin")
+
+Plug("nvim-treesitter/nvim-treesitter")
+Plug("nvim-tree/nvim-tree.lua")
+Plug("numToStr/Comment.nvim")
+Plug("windwp/nvim-autopairs")
+Plug("sainnhe/gruvbox-material")
+
+vim.call("plug#end")
+
+require("config")
+require("plugins.nvim-tree")
+require("plugins.treesitter")
+require("plugins.comment")
+require("plugins.autopairs")
+require("plugins.gruvbox-material")
---
+++ .config/nvim/lua/plugins/gruvbox-material.lua
@@ -0,0 +1,6 @@
+vim.opt.termguicolors = true
+vim.opt.background = 'dark'
+vim.g.gruvbox_material_enable_italic = true
+vim.g.gruvbox_material_better_performance = false
+vim.g.gruvbox_material_background = 'medium'
+vim.cmd.colorscheme('gruvbox-material')
