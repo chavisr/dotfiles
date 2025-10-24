80d13552dcc459e354b70d9ee00dbe5102d239a6
Chavis Rujichan <chavis.rujichan@gmail.com>
2025-10-24 11:15:57 +0700

    update

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
-  -- or                              , branch = '0.1.x',
+  -- tag = '0.1.8',
+  branch = '0.1.x',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     vim.api.nvim_set_hl(0, 'TelescopeSelection', { bg = '#45403d', fg = '#D8A657' })
