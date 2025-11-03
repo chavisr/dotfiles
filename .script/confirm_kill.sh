a34577842db25527b228fb9f470434e2cb00ed85
Chavis Rujichan <chavis.rujichan@gmail.com>
2025-11-03 14:42:32 +0700

    update

---
+++ .script/confirm_kill.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+CONFIRM_FILE='/tmp/confirm_close'
+DUNST_COUNT=$(dunstctl count | grep -i current | awk '{print $3}')
+
+if [ "$DUNST_COUNT" -eq 0 ]; then
+  rm "$CONFIRM_FILE"
+fi
+
+if [ ! -f "$CONFIRM_FILE" ]; then
+  touch "$CONFIRM_FILE"
+  dunstify --urgency critical --action='closeAction,close' 'Killing Focused Window ⚠️' \
+    | grep -q closeAction && bspc node -k
+else
+  dunstctl action
+  rm "$CONFIRM_FILE"
+fi
