f5c4290d36b472ca5916948a631f13a7133fa4d3
Chavis Rujichan <chavis.rujichan@gmail.com>
2025-11-19 16:36:40 +0700

    update

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,25 +1,25 @@
 return {
-	'neovim/nvim-lspconfig',
-	config = function()
-		vim.lsp.enable({
-			"bashls",
-			"lua_ls",
-			"zls",
-			"pyright",
-      "yamlls",
-      "jsonls",
-		})
-		vim.diagnostic.config({
-			-- virtual_lines = true,
-			virtual_text = true,
-			underline = true,
-			-- update_in_insert = false,
-			severity_sort = true,
-			float = {
-				-- style = 'minimal',
-				border = "rounded",
-				-- source = true,
-			},
-		})
-	end
+  'neovim/nvim-lspconfig',
+  config = function()
+    -- vim.lsp.enable({
+    -- 	"bashls",
+    -- 	"lua_ls",
+    -- 	"zls",
+    -- 	"pyright",
+    --  "yamlls",
+    --  "jsonls",
+    -- })
+    vim.diagnostic.config({
+      -- virtual_lines = true,
+      virtual_text = true,
+      underline = true,
+      -- update_in_insert = false,
+      severity_sort = true,
+      float = {
+        -- style = 'minimal',
+        border = "rounded",
+        -- source = true,
+      },
+    })
+  end
 }
