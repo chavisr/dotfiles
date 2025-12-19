aa390ddc62daaee9bc5e0a0a63efc1614cb7ff85
Chavis Rujichan <chavis.rujichan@gmail.com>
2025-12-19 17:59:48 +0700

    update

--- .script/confirm_kill.sh
+++ .script/confirm_kill.sh
@@ -9,7 +9,8 @@ fi
 
 if [ ! -f "$CONFIRM_FILE" ]; then
   touch "$CONFIRM_FILE"
-  dunstify --urgency critical --action='closeAction,close' 'Killing Focused Window ⚠️' \
+  dunstify --timeout 5000 --urgency critical \
+    --action='closeAction,close' 'Killing Focused Window ⚠️' \
     | grep -q closeAction && bspc node -k
 else
   dunstctl action
