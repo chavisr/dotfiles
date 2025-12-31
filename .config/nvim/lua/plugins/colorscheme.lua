6ab3c68d6f9d1259a8b6d567cb97f095332ca24e
Chavis Rujichan <chavis.rujichan@gmail.com>
2025-12-31 12:56:19 +0700

    update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 local function enable_transparency()
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  -- vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-  -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+  -- vim.api.nvim_set_hl(0, "LineNr",       { bg = "none" })
+  -- vim.api.nvim_set_hl(0, "NormalNC",    { bg = "none" })
   -- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+  -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+  -- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 end
 
 return {
@@ -13,13 +16,13 @@ return {
   config = function()
     vim.g.gruvbox_material_enable_italic = true
     vim.g.gruvbox_material_better_performance = false
-    vim.g.gruvbox_material_foreground = "material" -- material/mix/original
-    vim.g.gruvbox_material_background = "hard" -- hard/soft/medium
-    vim.g.gruvbox_material_float_style = "blend" -- dim/bright/blend
-    vim.g.gruvbox_material_ui_contrast = "low" -- low/high
+    vim.g.gruvbox_material_foreground = "material"     -- material/mix/original
+    vim.g.gruvbox_material_background = "hard"         -- hard/soft/medium
+    vim.g.gruvbox_material_float_style = "blend"       -- dim/bright/blend
+    vim.g.gruvbox_material_ui_contrast = "low"         -- low/high
     vim.g.gruvbox_material_statusline_style = "custom" -- default/mix/original/custom
 
     vim.cmd.colorscheme('gruvbox-material')
-    -- enable_transparency()
+    enable_transparency()
   end
 }
