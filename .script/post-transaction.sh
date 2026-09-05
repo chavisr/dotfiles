f0f09e89070995bced5a4434543032a52f2a6db0
Chavis Rujichan <chavis.rujichan@gmail.com>
2026-09-05 17:20:10 +0700

    update

--- .script/post-transaction.sh
+++ .script/post-transaction.sh
@@ -4,6 +4,3 @@
 pacman -Qqne > /home/chavi/.config/pkglist.txt
 pacman -Qqme > /home/chavi/.config/foreignpkglist.txt
 echo 'Packages list generated.'
-
-# Fix input rate bug after installing packages (x11 only)
-# xset r rate 300 50
